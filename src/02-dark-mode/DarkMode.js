a829149bae8fc45799d16a19f2d7cb482f76987f
Andres Della <andres.dellaporta@globallogic.com>
2023-10-30 17:10:16 -0300

    Adding my solution

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,21 @@
-export default function DarkMode () {
-  return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
-    </div>
-  )
+import React from 'react';
+class DarkMode extends React.Component {
+  state = {
+    isEnabled: false
+  }
+  handleChangeMode = (synthethicEvent) => {
+    const isDarkModeEnabled = synthethicEvent.target.name == 'darkButton' ? true : false;
+    this.setState({
+      isEnabled: isDarkModeEnabled
+    });
+  }
+  render() {
+    return (
+      <div className={'page'.concat(this.state.isEnabled ? ' dark-mode' : '')}>
+        <button className='dark-mode-button' name='darkButton' onClick={this.handleChangeMode}>Dark Mode</button>
+        <button className='light-mode-button' name='lightButton' onClick={this.handleChangeMode}>Light Mode</button>
+      </div>
+    );
+  }
 }
+export default DarkMode;
