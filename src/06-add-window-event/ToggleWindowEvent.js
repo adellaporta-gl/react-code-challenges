6420763e6c373fa50ffbc60ae48f24879c65c3f1
adellaporta <adellaporta@hexacta.com>
2023-11-01 12:07:28 -0300

    Adding my solution

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import React from 'react'
 import WindowEvent from './WindowEvent'
 
-export default function ToggleWindowEvent () {
-  const [windowEvent, setWindowEvent] = useState(false)
-  return (
-    <div>
-      <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
-      {windowEvent && <WindowEvent />}
-    </div>
-  )
+class ToggleWindowEvent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowEvent: false
+    }
+    window.addEventListener('dblclick', this.handleWindowDoubleClick);
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleWindowEvent}>Toggle Window Event</button>
+        {this.state.windowEvent && <WindowEvent />}
+      </div>
+    );
+  }
+  handleWindowEvent = (syntheticEvent) => {
+    this.setState(
+      function (prevState, props) {
+        const prevWindowEvent = prevState.windowEvent;
+        return {
+          windowEvent: !prevWindowEvent
+        }
+      }
+    );
+  }
+  handleWindowDoubleClick = () => {
+    if (this.state.windowEvent) {
+      alert('you double clicked the current window');
+    }
+  }
 }
+export default ToggleWindowEvent;
