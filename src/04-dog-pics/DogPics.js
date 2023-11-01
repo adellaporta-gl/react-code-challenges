b527d48adb55811b0249ef014ddd42cdce29b155
adellaporta <adellaporta@hexacta.com>
2023-11-01 10:43:19 -0300

    Adding my solution

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,41 @@
-export default function DogPics () {
-  // API: https://dog.ceo/dog-api/
-  return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
-    </div>
-  )
+import React from 'react';
+class DogPics extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgSrc: 'https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg'
+    }
+    this.changeDogImage = this.changeDogImage.bind(this);
+  }
+  changeDogImage() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(jsonResponse => {
+        console.log('jsonResponse: \n', jsonResponse);
+        this.setState({
+          imgSrc: jsonResponse.message
+        })
+      })
+
+  }
+  render() {
+    return (
+      <div className='dog-pics'>
+        <img src={this.state.imgSrc} />
+        <button onClick={this.changeDogImage}>🐶</button>
+      </div>
+    );
+  }
 }
+export default DogPics;
+
+// export default function DogPics() {
+//   // API: https://dog.ceo/dog-api/
+//   //
+//   return (
+//     <div className='dog-pics'>
+//       <img src='https://dog.ceo/api/breeds/image/random' />
+//       <button>🐶</button>
+//     </div>
+//   )
+// }
