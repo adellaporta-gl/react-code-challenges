2090b1b53b8d1b2a3a6039cfe77968b55204fd20
adellaporta <adellaporta@hexacta.com>
2023-11-01 13:23:01 -0300

    Adding my solution

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,34 +1,76 @@
 import React from 'react'
 
-function ColorPicker () {
-  const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
-  return (
-    <div>
-      <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} />)}
-    </div>
-  )
+class ColorPicker extends React.Component {
+  render() {
+    const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
+    return (
+      <div>
+        <h1>Choose a color</h1>
+        {colors.map(color => <button key={color} style={{ backgroundColor: color }} onClick={this.handleClick} value={color} />)}
+      </div>
+    );
+  }
+  handleClick = (syntheticEvent) => {
+    this.props.colorHandler(syntheticEvent.target.value);
+  }
 }
 
-function Pixel () {
-  return <div style={{ height: '20px', width: '20px', backgroundColor: 'lightGrey', margin: '1px' }} />
+class Pixel extends React.Component {
+  state = {
+    pixelColor: 'lightgray'
+  }
+  render() {
+    return (
+      <div
+        style={{
+          height: '20px',
+          width: '20px',
+          backgroundColor: this.state.pixelColor,
+          margin: '1px'
+        }}
+        onClick={this.pixelHandler}
+      />);
+  }
+  pixelHandler = () => {
+    this.setState({
+      pixelColor: this.props.color
+    })
+  }
 }
 
-function Pixels () {
-  const pixels = []
-  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />)
-  return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', width: '210px', margin: '0 auto' }}>
-      {pixels}
-    </div>
-  )
+class Pixels extends React.Component {
+  render() {
+    const pixels = []
+    for (let i = 0; i < 100; i++) {
+      pixels.push(<Pixel key={i} color={this.props.selectedColor} />)
+    }
+    return (
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', width: '210px', margin: '0 auto' }}>
+        {pixels}
+      </div>
+    );
+  }
 }
 
-export default function PixelArt () {
-  return (
-    <div>
-      <ColorPicker />
-      <Pixels />
-    </div>
-  )
+class PixelArt extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'lightgray'
+    }
+  }
+  render() {
+    return (
+      <div>
+        <ColorPicker colorHandler={this.handleColorSelection} />
+        <Pixels selectedColor={this.state.color} />
+      </div>
+    );
+  }
+  handleColorSelection = (color) => {
+    this.setState({
+      color: color
+    });
+  }
 }
+export default PixelArt;
